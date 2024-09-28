dfce56e0f953d5625da1c3c9576622f4801d83ee
Vegard Lokreim <vegardlokreim9@gmail.com>
2024-09-28 14:09:47 +0200

    Updated type for CreateEmployee

--- src/types/backend/firestoreTypes.ts
+++ src/types/backend/firestoreTypes.ts
@@ -1,5 +1,5 @@
 
-import { Firestore, Timestamp } from "firebase-admin/firestore";
+import { Timestamp } from "firebase-admin/firestore";
 
 
 export type Weekday = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday"
